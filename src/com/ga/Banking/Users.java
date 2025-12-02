2d4b91237667b3b9cdbc63bec0b23e5bfdb095f6
MrDPirate <khalil.ak.bh1170@gmail.com>
2025-12-02 18:38:09 +0300

    first commit, added the user stories and Users class

---
+++ src/com/ga/Banking/Users.java
@@ -0,0 +1,90 @@
+package com.ga.Banking;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Users {
+    private String username;
+    private String password;
+    private String role;
+    private int failed_login_attempts;
+    private LocalDateTime account_locked_until;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+    private long id;
+
+    public Users(String username, String password, String role, LocalDateTime created_at, long id) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.failed_login_attempts = 0;
+        this.account_locked_until= created_at;
+        this.created_at = created_at;
+        this.updated_at = created_at;
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getFailed_login_attempts() {
+        return failed_login_attempts;
+    }
+
+    public void setFailed_login_attempts(int failed_login_attempts) {
+        this.failed_login_attempts = failed_login_attempts;
+    }
+
+    public LocalDateTime getAccount_locked_until() {
+        return account_locked_until;
+    }
+
+    public void setAccount_locked_until(LocalDateTime account_locked_until) {
+        this.account_locked_until = account_locked_until;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
